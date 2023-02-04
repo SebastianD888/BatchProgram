ac9e6cbc743728f8cf98e80f945d9bb471c14714
Sebastian <decker-sebastian@web.de>
2023-02-04 11:51:20 +0100

    update display name of database items

--- batchprogram/batch/models.py
+++ batchprogram/batch/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Enter_Leave(models.Model):
-    enter = models.CharField(max_length=100)
-    leave = models.CharField(max_length=100)
+    enter = models.CharField(verbose_name="Enter-Time", max_length=100)
+    leave = models.CharField(verbose_name="Leave-Time", max_length=100)
+    def __str__(self):
+        return f'from: {self.enter}, until: {self.leave}'
--- batchprogram/batch/test.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime, timedelta, time
-now = datetime.now() # save current time to now parameter
-current_time = now.strftime("%m/%d/%Y, %H:%M:%S") # change time format
-currenttime={str(current_time)}
