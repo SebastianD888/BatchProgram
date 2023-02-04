3f5fff27a788d4995cb964583be3a6348820bcc7
Sebastian <decker-sebastian@web.de>
2023-02-04 12:32:08 +0100

    add working time to database item name

--- batchprogram/batch/models.py
+++ batchprogram/batch/models.py
@@ -5,4 +5,4 @@ class Enter_Leave(models.Model):
     enter = models.CharField(verbose_name="Enter-Time", max_length=100)
     leave = models.CharField(verbose_name="Leave-Time", max_length=100)
     def __str__(self):
-        return f'from: {self.enter}, until: {self.leave}'
+        return f'working time from: {self.enter} until: {self.leave}'
